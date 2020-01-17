0b4da8d343025ae646f4ae842571d5a3b9c82a25
陳彥 <iris8822@chenyande-MacBook-Pro.local>
2020-01-17 20:25:56 +0800

    Modified build script and send_file

--- build_server.sh
+++ build_server.sh
@@ -3,6 +3,9 @@ cd src
 if [ ! -d env/ ]; then
     wget http://csie.ntu.edu.tw/~b08902009/env-server.zip
     unzip env-server.zip; rm -rf env-server.zip
+    source env-server/bin/activate
+else
+	source env/bin/activate
 fi
-source env-server/bin/activate
+
 python3 server.py
---
+++ src/data/bob/henry/test(1).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a=0,b=2;
+	if((a||b)>=2)
+		printf("kokoko\n");
+	b=0||0;
+	printf("%d",b);
+	return 0;
+}
