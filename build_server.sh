b4e4dd7fe75d4054489a95e2ef5d0a7423ae00c9
stupid <iammandy0104@gmail.com>
2020-01-18 00:48:46 +0800

    build.sh

--- build_server.sh
+++ build_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd src
 if [ ! -d env-server/ ]; then
-    wget http://csie.ntu.edu.tw/~b08902009/env-server.zip
+    wget http://csie.ntu.edu.tw/~b08902009/env-server.zip || curl -OL http://csie.ntu.edu.tw/~b08902009/env-server.zip
     unzip env-server.zip; rm -rf env-server.zip
     source env-server/bin/activate
 else
